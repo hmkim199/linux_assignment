3dcdb309c4cf2e84b03f5414ccad9820cc37dacd
hmkim199 <hmkim199@gmail.com>
2020-10-01 19:34:54 +0900

    change input to parameter

--- multi-table.sh
+++ multi-table.sh
@@ -2,25 +2,22 @@
 
 # 20172510 hyemin Kim assignment
 
-echo -n "input rows and columns : "
-read rows columns
-
 re="^[0-9]+$"
-if ! [[ $rows =~ $re && $columns =~ $re ]] ; then       	
+if ! [[ $1 =~ $re && $2 =~ $re ]] ; then       	
 	echo "error: Not a number" >&2
 	exit 1
 fi
 
-if [ ${rows} -lt 1 ] || [ ${columns} -lt 1 ] ; then
+if [ $1 -lt 1 ] || [ $2 -lt 1 ] ; then
         echo "wrong input : you have to input number greater than 0."
         exit 0
 fi
 
-for row in $(seq 1 $rows)
+for row in $(seq 1 $1)
 do
-        for column in $(seq 1 $columns)
+        for column in $(seq 1 $2)
         do
-                echo ${row}"*"${column}"="`expr $row \* $column`
+                echo $1"*"$2"="`expr $row \* $column`
         done
         echo ""
 done
